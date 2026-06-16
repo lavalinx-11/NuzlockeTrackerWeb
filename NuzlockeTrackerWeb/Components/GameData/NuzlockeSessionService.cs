b8c7b0db92fca99e58bfcf0667556a5e6fef0828
VALIPC\laval <zahirlawrence2@gmail.com>
2026-06-16 12:45:25 -0400

    Optimized code and added new section for stats page

--- NuzlockeTrackerWeb/Components/GameData/NuzlockeSessionService.cs
+++ NuzlockeTrackerWeb/Components/GameData/NuzlockeSessionService.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace NuzlockeTrackerWeb.Components.GameData
 {
@@ -13,38 +10,20 @@ namespace NuzlockeTrackerWeb.Components.GameData
     }
     //  MATCH DATA 
     public class MatchResult {
-        public int Id { get; set; } 
-        public DateTime MatchDate { get; set; } = DateTime.UtcNow; 
-        public string Team1Key { get; set; } = "";
-        public string Team2Key { get; set; } = "";
-        public List<string> Team1Names { get; set; } = new();
-        public List<string> Team2Names { get; set; } = new();
-        public List<string> Team1Roster { get; set; } = new();
-        public List<string> Team2Roster { get; set; } = new();
-        public List<string> BanList { get; set; } = new();
-        public int Team1Rounds { get; set; }
-        public int Team2Rounds { get; set; }
-        public int WinningTeamSide { get; set; }
+        public int Id { get; init; } 
+        public DateTime MatchDate { get; init; } = DateTime.UtcNow; 
+        public string Team1Key { get; init; } = "";
+        public string Team2Key { get; init; } = "";
+        public List<string> Team1Names { get; init; } = new();
+        public List<string> Team2Names { get; init; } = new();
+        public List<string> Team1Roster { get; init; } = new();
+        public List<string> Team2Roster { get; init; } = new();
+        public List<string> BanList { get; init; } = new();
+        public int Team1Rounds { get; init; }
+        public int Team2Rounds { get; init; }
+        public int WinningTeamSide { get; init; }
     }
-
-    // Stats Per Player
-    public class PlayerStat {
-        public string Name { get; set; } = "";
-        public int Wins { get; set; }
-        public int Losses { get; set; }
-        public int CurrentStreak { get; set; }
-        public List<bool> RecentForm { get; set; } = new();
-        public int Ratio => (Wins + Losses) == 0 ? 0 : (int)((double)Wins / (Wins + Losses) * 100);
-    }
-
-    // Stats Per Team
-    public class TeamStat {
-        public string TeamComposition { get; set; } = "";
-        public int Wins { get; set; }
-        public int Losses { get; set; }
-        public int Ratio => (Wins + Losses) == 0 ? 0 : (int)((double)Wins / (Wins + Losses) * 100);
-    }
-
+    
     //  Live Match Data (Used While Playing Actively) 
     public class GameSession {
         public string Id { get; set; } = Guid.NewGuid().ToString()[..6].ToUpper();
@@ -53,13 +32,14 @@ namespace NuzlockeTrackerWeb.Components.GameData
         public List<Player> Team2 { get; set; } = new();
         public List<Player> PickingOrder { get; set; } = new();
         public List<string> CurrentBans { get; set; } = new();
+        // ReSharper disable once CollectionNeverUpdated.Global
         public List<MatchResult> MatchHistory { get; set; } = new();
-        public int CurrentPickerIndex { get; set; } = 0;
-        public int WinningTeamNumber { get; set; } = 0;
-        public bool HistoryRecorded { get; set; } = false;
+        public int CurrentPickerIndex { get; set; }
+        public int WinningTeamNumber { get; set; }
+        public bool HistoryRecorded { get; set; }
         public string RivalryText { get; set; } = "";
         public List<Player> UnassignedPlayers { get; set; } = new();
-        public event Action OnChange;
+        public event Action? OnChange;
         public string HostId { get; set; } = ""; 
         public void Notify() => OnChange?.Invoke();
     }
